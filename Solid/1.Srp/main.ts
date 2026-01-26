18ddd63d6663e8815e47792a0e984a7beafdeb71
grace-luong-goldenowl <grace.luong.gos@gmail.com>
2026-01-26 10:47:58 +0700

    feat: do srp exercise

--- Solid/1.Srp/main.ts
+++ Solid/1.Srp/main.ts
@@ -8,62 +8,22 @@
  * Happy coding! 🚀
  */
 
-// class Book {
-//     title: string;
-//     author: string;
-//     publicationYear: number;
-//     constructor(title: string, author: string, publicationYear: number) {
-//         this.title = title;
-//         this.author = author;
-//         this.publicationYear = publicationYear;
-//     }
-// }
+import { Book } from "./book";
+import { Library } from "./library";
+import { BookSearch } from "./bookSearch";
 
-// class Library {
-//     books: Book[];
+let lib: Library;
+lib = new Library();
 
-//     constructor() {
-//         this.books = [];
-//     }
-//     addBook(book: Book): void {
-//         this.books.push(book);
-//     }
+let book1: Book;
+book1 = new Book("Clean Code", "Edric Cao", 2023);
 
-//     getListBooks(): Book[] {
-//         return this.books;
-//     }
+let book2: Book;
+book2 = new Book("Design Pattern", "Edric Cao", 2022);
 
-//     removeBook(title: string): void {
-//         this.books = this.books.filter(book => book.title !== title);
-//     }
+lib.addBook(book1);
+lib.addBook(book2);
 
-//     getBookByTitle(title: string): Book | undefined {
-//         return this.books.find(book => book.title === title);
-//     }
-
-//     getTotalNumberOfBooks(): number {
-//         return this.books.length;
-//     }
-
-//     getBooksByAuthor(author: string): Book[] {
-//         return this.books.filter(book => book.author === author);
-//     }
-
-//     getBooksByPublicationYear(publicationYear: number): Book[] {
-//         return this.books.filter(book => book.publicationYear === publicationYear);
-//     }
-// }
-
-// let lib: Library;
-// lib = new Library();
-
-// let book1: Book;
-// book1 = new Book('Clean Code', 'Edric Cao', 2023);
-
-// let book2: Book;
-// book2 = new Book('Design Pattern', 'Edric Cao', 2022);
-
-// lib.addBook(book1);
-// lib.addBook(book2);
-// console.log(lib.getBookByTitle('Clean Code')); // Output: Book { title: 'Clean Code', author: 'Edric Cao', publicationYear: 2023 }
-
+let search: BookSearch;
+search = new BookSearch([book1, book2]);
+console.log(search.getBookByTitle("Clean Code")); // Output: Book { title: 'Clean Code', author: 'Edric Cao', publicationYear: 2023 }
